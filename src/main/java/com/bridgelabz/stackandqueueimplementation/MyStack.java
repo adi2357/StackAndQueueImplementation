ff65f7ad85cb5e6dd2dbcadf5ab40f78aade0f75
Aditya Verma <aaadarths2357@gmail.com>
2020-10-07 21:31:07 +0530

    Added: create stack with push method internally calling add method of linked list

---
+++ src/main/java/com/bridgelabz/stackandqueueimplementation/MyStack.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.stackandqueueimplementation;
+
+public class MyStack {
+	private final MyLinkedList myLinkedList;
+	
+	public MyStack() {
+		this.myLinkedList =new MyLinkedList();
+	}
+
+	public void push(AllNode newNode) {
+		myLinkedList.add(newNode);
+		
+	}
+
+	public void printStack() {
+		myLinkedList.printMyList();
+		
+	}
+	
+	public AllNode peek() {
+		return myLinkedList.getHead();
+	}
+	
+	
+
+}
---
+++ src/test/java/com/bridgelabz/stackandqueueimplementation/MyStackTest.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.stackandqueueimplementation;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import junit.framework.Assert;
+
+public class MyStackTest {
+
+	@Test
+	public void given3NumberWhenAddedToStackShouldHaveLastAddedNodeAtTop() {
+		MyNode<Integer> firstNode = new MyNode(70);
+		MyNode<Integer> secondNode = new MyNode(30);
+		MyNode<Integer> thirdNode = new MyNode(56);
+
+		MyStack myStack = new MyStack();
+		myStack.push(firstNode);
+		myStack.push(secondNode);
+		myStack.push(thirdNode);
+		
+		AllNode top=myStack.peek();
+		myStack.printStack();
+		
+		Assert.assertEquals(thirdNode,top);
+	}
+
+}
